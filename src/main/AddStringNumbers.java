47fbf8d032b2eb2bc749f7ca2a946b456112cf2e
unknown <ivankshu@gmail.com>
2022-01-30 12:38:44 +0530

    02. null String should return IllegalArgumentException

---
+++ src/main/AddStringNumbers.java
@@ -0,0 +1,9 @@
+package main;
+
+public class AddStringNumbers {
+	public int add(String numbersStr) {
+		if (numbersStr == null)
+			throw new IllegalArgumentException("Null String is not allow");
+		return 0;
+	}
+}
--- src/test/TestApplication.java
+++ src/test/TestApplication.java
@@ -1,13 +1,26 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 import org.junit.Test;
 
+import main.AddStringNumbers;
+
+
 public class TestApplication {
+	private AddStringNumbers addNumber = new AddStringNumbers();
+	
 	@Test
 	public void testingJUnit() {
 		String str = "testing JUnit";
 		assertEquals("testing JUnit", str);
+	}
+	
+	@Test
+	public void nullStringReturnIllegalArgumentException() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			addNumber.add(null);
+		});
 	}
 }
