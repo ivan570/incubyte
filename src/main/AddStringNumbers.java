30a3d9e47d00041eb122589faa59ef8e6418bea5
unknown <ivankshu@gmail.com>
2022-01-30 12:42:45 +0530

    03. empty String should return Zero

--- src/main/AddStringNumbers.java
+++ src/main/AddStringNumbers.java
@@ -4,6 +4,11 @@ public class AddStringNumbers {
 	public int add(String numbersStr) {
 		if (numbersStr == null)
 			throw new IllegalArgumentException("Null String is not allow");
+		
+		// if string is empty return 0
+		if (numbersStr.equals(""))
+			return 0;
+		
 		return 0;
 	}
 }
--- src/test/TestApplication.java
+++ src/test/TestApplication.java
@@ -7,20 +7,24 @@ import org.junit.Test;
 
 import main.AddStringNumbers;
 
-
 public class TestApplication {
 	private AddStringNumbers addNumber = new AddStringNumbers();
-	
+
 	@Test
 	public void testingJUnit() {
 		String str = "testing JUnit";
 		assertEquals("testing JUnit", str);
-	}
-	
+	} // testing JUnit connection
+
 	@Test
 	public void nullStringReturnIllegalArgumentException() {
 		assertThrows(IllegalArgumentException.class, () -> {
 			addNumber.add(null);
 		});
-	}
+	} // null String should return IllegalArgumentException
+
+	@Test
+	public void emptyStringReturnZero() {
+		assertEquals(0, addNumber.add(""));
+	} // empty String should return Zero
 }
